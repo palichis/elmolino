830a3d4b4c1177c0a6ebfcecbafbc92201110c5d
root <root@palichis.(none)>
2015-01-22 19:56:36 -0500

    agregando proyecto django

---
+++ web/migrations/0001_initial.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='usuario',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('usuario', models.CharField(max_length=10)),
+                ('clave', models.CharField(max_length=10)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
