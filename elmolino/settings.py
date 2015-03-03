2aa81f13512ae205ffeb37c17565fac31b485a10
palichis <palichis@solid-ec.org>
2015-03-03 10:01:27 -0500

    actualizacion de db

--- elmolino/settings.py
+++ elmolino/settings.py
@@ -75,10 +75,10 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
             'NAME': 'elmolino',                      # Or path to database file if using sqlite3.
             # The following settings are not used with sqlite3:
-            'USER': 'elmolino',		
+            'USER': 'elmolino',
             'PASSWORD': '12345678',
             'HOST': 'localhost',                      # Empty for localhost throu
-	    'PORT': '5432', 
+	    'PORT': '5432',
     }
 }
 
